4d70823358135ab939005efec251f3340a821aa0
Bheema <bheemudu.guguloth@travelxp.tv>
2023-08-23 17:22:46 +0530

    project setup done

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="">
+      <span className="text-[20px] text-green-500 flex justify-center items-center h-[100vh]">
+        Home Page
+      </span>
+    </div>
+  )
+}
